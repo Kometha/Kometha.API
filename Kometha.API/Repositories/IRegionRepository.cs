16585fd2427ca3fd2b2d0265f2471547303f38ea
Kometha <kenethcubas2002@unitec.edu>
2025-07-25 22:39:54 -0600

    Añadir interfaz IRegionRepository y su implementación
    Se ha creado la interfaz `IRegionRepository` en `IRegionRepository.cs`, que incluye un método comentado `GetAllAsync`. Además, se ha implementado la clase `SQLRegionRepository` en `SQLRegionRepository.cs`, que implementa la interfaz y proporciona un método asíncrono para obtener todas las regiones desde la base de datos utilizando `KomethaDBContext`.

---
+++ Kometha.API/Repositories/IRegionRepository.cs
@@ -0,0 +1,11 @@
+﻿using Kometha.API.Models.Domain;
+
+namespace Kometha.API.Repositories
+{
+    public interface IRegionRepository
+    {
+        //
+        Task<List<Region>> GetAllAsync();
+        //
+    }
+}
---
+++ Kometha.API/Repositories/SQLRegionRepository.cs
@@ -0,0 +1,20 @@
+﻿using Kometha.API.Dataa;
+using Kometha.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kometha.API.Repositories
+{
+    public class SQLRegionRepository : IRegionRepository
+    {
+        private readonly KomethaDBContext dBContext;
+        public SQLRegionRepository(KomethaDBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+
+        public async Task<List<Region>> GetAllAsync()
+        {
+            return await dBContext.Regions.ToListAsync();
+        }
+    }
+}
