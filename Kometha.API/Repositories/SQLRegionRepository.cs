613bf016174efac622b644e3b1264c2b59c7839e
Kometha <kenethcubas2002@unitec.edu>
2025-07-27 21:29:32 -0600

    Actualiza repositorio de regiones con métodos CRUD
    Se ha modificado la interfaz `IRegionRepository` para incluir métodos para crear, actualizar, eliminar y obtener regiones por ID, eliminando el método comentado `GetAllAsync()`.
    
    En la clase `SQLRegionRepository`, se han implementado los métodos `CreateAsync`, `UpdateAsync`, `DeleteAsync`, `GetAllAsync` y `GetByIdAsync`, que permiten realizar operaciones CRUD asíncronas sobre las regiones utilizando el contexto de la base de datos `KomethaDBContext`.

--- Kometha.API/Repositories/SQLRegionRepository.cs
+++ Kometha.API/Repositories/SQLRegionRepository.cs
@@ -12,9 +12,54 @@ namespace Kometha.API.Repositories
             this.dBContext = dBContext;
         }
 
+        public async Task<Region> CreateAsync(Region region)
+        {
+            await dBContext.Regions.AddAsync(region);
+            await dBContext.SaveChangesAsync();
+            return region;
+        }
+
+        public async Task<Region> UpdateAsync(Guid id, Region region)
+        {
+            var existingRegion = await dBContext.Regions.FindAsync(id);
+
+            if (existingRegion == null) 
+            {
+                return null;            
+            }
+
+            existingRegion.Code = region.Code;
+            existingRegion.Name = region.Name;
+            existingRegion.RegionImageUrl = region.RegionImageUrl;
+
+            await dBContext.SaveChangesAsync();
+            return existingRegion;
+
+        }
+
+        public async Task<Region?> DeleteAsync(Guid id)
+        {
+            var existingRegion = await dBContext.Regions.FindAsync(id);
+
+            if (existingRegion == null) 
+            {
+                return null;
+            }
+
+            dBContext.Regions.Remove(existingRegion);
+            await dBContext.SaveChangesAsync();
+
+            return existingRegion;
+        }
+
         public async Task<List<Region>> GetAllAsync()
         {
             return await dBContext.Regions.ToListAsync();
+        }
+
+        public async Task<Region?> GetByIdAsync(Guid id)
+        {
+            return await dBContext.Regions.FindAsync(id);
         }
     }
 }
