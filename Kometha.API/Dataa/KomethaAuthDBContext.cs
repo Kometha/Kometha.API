9381c774aefdefaa0fe64c6a6e3b3264d584352a
Kometha <kenethcubas2002@unitec.edu>
2025-10-26 22:24:23 -0600

    Add role seeding for Reader and Writer roles
    Updated KomethaAuthDBContext to seed Reader and Writer roles
    using the OnModelCreating method. Added a `using` directive
    for Microsoft.AspNetCore.Identity to support IdentityRole.
    Seeded roles include predefined IDs, names, and normalized
    names using the HasData method.

--- Kometha.API/Dataa/KomethaAuthDBContext.cs
+++ Kometha.API/Dataa/KomethaAuthDBContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace Kometha.API.Dataa
 {
@@ -7,6 +8,35 @@ namespace Kometha.API.Dataa
     {
         public KomethaAuthDBContext(DbContextOptions<KomethaAuthDBContext> options) : base(options)
         {
-        }        
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "d5613a61-a75a-4e80-9be5-86e4bf14b8ea";
+
+            var writerRoleId = "e0ae86d3-0ba6-426e-a886-9bb075e7f449";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()                    
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
