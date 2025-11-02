c448fa9feebd1219c0897fdef850b47318413e0a
Kometha <kenethcubas2002@unitec.edu>
2025-11-01 19:55:35 -0600

    Add Identity and JWT authentication services
    Configured KomethaAuthDBContext to use SQL Server.
    Integrated Identity services with role management, token providers, and default token configurations.
    Enforced password policies and unique email requirements for users.
    Implemented JWT authentication with token validation parameters, including issuer, audience, and signing key.
    Enabled role-based access control and secure authentication mechanisms.

---
+++ Kometha.API/Controllers/AuthController.cs
@@ -0,0 +1,74 @@
+﻿namespace Kometha.API.Controllers
+{
+    using Kometha.API.Models.DTOs;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// Defines the <see cref="AuthController" />
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        /// <summary>
+        /// Defines the userManager
+        /// </summary>
+        private readonly UserManager<IdentityUser> userManager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AuthController"/> class.
+        /// </summary>
+        /// <param name="userManager">The userManager<see cref="UserManager{IdentityUser}"/></param>
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            UserManager = userManager;
+        }
+
+        /// <summary>
+        /// Gets the UserManager
+        /// </summary>
+        public UserManager<IdentityUser> UserManager { get; }
+
+        // POST: api/Auth/Register
+
+        /// <summary>
+        /// The Register
+        /// </summary>
+        /// <param name="registerRequestDto">The registerRequestDto<see cref="RegisterRequestDto"/></param>
+        /// <returns>The <see cref="Task{IActionResult}"/></returns>
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerRequestDto.Username,
+                Email = registerRequestDto.Username,
+            };
+
+            var identityResult = await UserManager.CreateAsync(identityUser, registerRequestDto.Password);
+
+            if (identityResult.Succeeded)
+            {
+                //Add roles to this User
+                if (registerRequestDto.Roles != null && registerRequestDto.Roles.Any())
+                {
+                    identityResult = await UserManager.AddToRolesAsync(identityUser, registerRequestDto.Roles);
+
+                    if (identityResult.Succeeded)
+                    {
+                        return Ok("Usuario registrado");
+                    }
+
+                }
+
+                return BadRequest(identityResult.Errors);
+            }
+            else
+            {
+                return BadRequest(identityResult.Errors);
+            }
+        }
+    }
+}
--- Kometha.API/Program.cs
+++ Kometha.API/Program.cs
@@ -2,9 +2,9 @@ using Kometha.API.Dataa;
 using Kometha.API.Mappings;
 using Kometha.API.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,11 +26,23 @@ builder.Services.AddScoped<IWalkRepository, SQLWalkRepository>();
 
 builder.Services.AddAutoMapper(cfg => { }, typeof(AutoMapperProfiles));
 
+builder.Services.AddIdentityCore<IdentityUser>().AddRoles<IdentityRole>().AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("Kometha").AddEntityFrameworkStores<KomethaAuthDBContext>().AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 9;
+    options.User.RequireUniqueEmail = true;
+});
+
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
 {
     ValidateIssuer = true,
     ValidateAudience = true,
-    ValidateLifetime =  true,
+    ValidateLifetime = true,
     ValidateIssuerSigningKey = true,
     ValidIssuer = builder.Configuration["Jwt:Issuer"],
     ValidAudience = builder.Configuration["Jwt:Audience"],
