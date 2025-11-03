6b4c6a6a65cd49161b2ff21bf118f61e06476356
Kometha <kenethcubas2002@unitec.edu>
2025-11-02 21:53:52 -0600

    Add ImagesController for image upload functionality
    Introduced a new ImagesController to handle image uploads via
    the `UploadImage` endpoint (`POST: api/Images/Upload`). Added
    file validation for allowed extensions (.jpg, .jpeg, .png, .gif)
    and size limits (15MB). Converted DTOs to domain models and
    delegated upload logic to the IImageRepository. Ensured modular
    design and adherence to API standards.

---
+++ Kometha.API/Controllers/ImagesController.cs
@@ -0,0 +1,62 @@
+﻿using Kometha.API.Models.Domain;
+using Kometha.API.Models.DTOs;
+using Kometha.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kometha.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImageRepository imageRepository;
+
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this.imageRepository = imageRepository;
+        }
+        // POST: api/Images/Upload
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> UploadImage([FromForm] ImageUploadRequestDTO request)
+        {
+            ValidateFileUpload(request);
+
+            if (ModelState.IsValid)
+            {
+                //Convert DTO to Domain Model
+                var imageDomainModel = new Image
+                {
+                    File = request.File,
+                    FileExtension = Path.GetExtension(request.File.FileName),
+                    FileSizeInBytes = request.File.Length,
+                    FileName = request.FileName,
+                    FileDescription = request.FileDescription,                    
+                };
+
+                // User repository logic to handle the file upload can be added here.
+                await imageRepository.Upload(imageDomainModel);
+
+                return Ok(imageDomainModel);
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        private void ValidateFileUpload(ImageUploadRequestDTO request)
+        {
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png", ".gif" };
+
+            if (allowedExtensions.Contains(Path.GetExtension(request.File.FileName)) == false)
+            {
+                ModelState.AddModelError("File", "Tipo de archivo no soportado.");             
+            }
+
+            //15728640 byte = 15MB
+            if (request.File.Length > 15728640)
+            {
+                ModelState.AddModelError("File", "El tamaño del archivo excede el límite permitido de 15MB.");
+            }
+        }
+    }
+}
