efb68044a1bb9a3c4083319b174b8e7fe6ac3f53
Kometha <kenethcubas2002@unitec.edu>
2025-11-02 21:55:25 -0600

    Add Images table and update schema with seed data
    Introduce a new `Images` table to the database schema with columns for metadata such as file name, size, and path. Update migration files and `DbContext` model snapshot to reflect these changes.
    
    Add seed data for `Difficulties` ("Easy," "Medium," "Hard") and `Regions` (with `Code`, `Name`, and optional `RegionImageUrl`). Define relationships between `Walk`, `Difficulty`, and `Region` entities with cascading delete behavior. Update `Walk` entity to include navigation properties.

--- Kometha.API/Migrations/20251102232756_Adding Images Table.Designer.cs
+++ Kometha.API/Migrations/20251102232756_Adding Images Table.Designer.cs
@@ -4,6 +4,7 @@ using Kometha.API.Dataa;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Kometha.API.Migrations
 {
     [DbContext(typeof(KomethaDBContext))]
-    partial class KomethaDBContextModelSnapshot : ModelSnapshot
+    [Migration("20251102232756_Adding Images Table")]
+    partial class AddingImagesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
