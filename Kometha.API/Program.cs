9d621e6081ed6cc7dda1309229ce5ca03ba1b057
Kometha <kenethcubas2002@unitec.edu>
2025-11-02 21:55:53 -0600

    Add static file support and image repository service
    - Added `Microsoft.Extensions.FileProviders` for static file handling.
    - Registered `IHttpContextAccessor` for accessing HTTP context.
    - Added `IImageRepository` with `LocalImageRepository` implementation.
    - Configured static file middleware to serve files from "Images" directory.
    - Enhanced application to support image management and static file serving.

--- Kometha.API/Program.cs
+++ Kometha.API/Program.cs
@@ -4,6 +4,7 @@ using Kometha.API.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
@@ -12,6 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpContextAccessor();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
@@ -59,6 +61,7 @@ builder.Services.AddDbContext<KomethaAuthDBContext>(options => options.UseSqlSer
 builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>();
 builder.Services.AddScoped<IWalkRepository, SQLWalkRepository>();
 builder.Services.AddScoped<ITokenRepository, TokenRepository>();
+builder.Services.AddScoped<IImageRepository, LocalImageRepository>();
 
 builder.Services.AddAutoMapper(cfg => { }, typeof(AutoMapperProfiles));
 
@@ -99,6 +102,14 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 
 app.UseAuthorization();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "Images")),
+    RequestPath = "/Images"
+
+});
 
 app.MapControllers();
 
