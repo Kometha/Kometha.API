e653ba8d335508c0eeda02facf42dafc8a57c947
Kometha <kenethcubas2002@unitec.edu>
2025-11-02 21:54:51 -0600

    Add DTO and domain model for image upload functionality
    Introduced `ImageUploadRequestDTO` in the `Kometha.API.Models.DTOs`
    namespace to handle image upload requests. This class includes
    properties for the uploaded file, file name, and optional file
    description.
    
    Added `Image` class in the `Kometha.API.Models.Domain` namespace
    to represent the image entity. This class includes properties
    for metadata such as file name, description, extension, size,
    and path. The `File` property is marked as `[NotMapped]` to
    exclude it from database persistence.
    
    These changes enable handling and storage of image upload
    requests in the application.

---
+++ Kometha.API/Models/Domain/Image.cs
@@ -0,0 +1,22 @@
+﻿namespace Kometha.API.Models.Domain
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Image
+    {
+        public Guid Id { get; set; }
+
+        [NotMapped]
+        public IFormFile File { get; set; }
+
+        public string? FileName { get; set; }
+
+        public string? FileDescription { get; set; }
+
+        public string FileExtension { get; set; }
+
+        public long FileSizeInBytes { get; set; }
+
+        public string FilePath { get; set; }
+    }
+}
